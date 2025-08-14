9f189e885e2fc1ea65c7ebd23cacb48076a33483
prabhjot-arora31 <prabhjotarora99@gmail.com>
2025-08-14 14:41:57 +0530

    new way

--- config/db.js
+++ config/db.js
@@ -1,17 +1,29 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  const uri = process.env.MONGODB_URI;
-  if (!uri) throw new Error("MONGODB_URI is not set");
-  await mongoose
-    .connect(uri, {
-      autoIndex: process.env.NODE_ENV !== "production",
-    })
-    .then(() => {
-      console.log("✅ MongoDB connected");
-    })
-    .catch((e) => {
-      console.log(`Error: ${e.message}`);
-    });
-};
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) throw new Error("MONGODB_URI is not set");
+
+    cached.promise = mongoose
+      .connect(uri, {
+        autoIndex: process.env.NODE_ENV !== "production",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
 export default connectDB;
